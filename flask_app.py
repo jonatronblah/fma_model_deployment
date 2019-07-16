4de7cd5d005f4cf36d7968f5e7d46837a42115c4
jonathan <39495158+jonatronblah@users.noreply.github.com>
2019-07-16 10:52:14 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -22,12 +22,17 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    genres = ['Hip-Hop', 'Pop', 'Rock', 'Experimental', 'Folk', 'Jazz',
+       'Electronic', 'Spoken', 'International', 'Soul-RnB', 'Blues',
+       'Country', 'Classical', 'Old-Time / Historic', 'Instrumental',
+       'Easy Listening']
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+        genre = str(request.form["genre"])
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
@@ -36,21 +41,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            df = compute_features(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #df.to_csv('test.csv')
-            values = df.values
+            dfu = compute_features(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            values = dfu.values
             values = values.reshape(1, -1)
-            pred = str(fma_pipe.predict(values))
+            pred = str(fma_pipe.predict(values)[0])
+            dfu = pd.DataFrame(values)
+            dfu['user_genre'] = genre
+            with open('/home/jonatron/website/fma_model/new_data.csv', 'a') as f:
+                dfu.to_csv(f, header=False, index=None)
+
             return pred
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('fma_upload.html', genres=genres)
 
 
 
